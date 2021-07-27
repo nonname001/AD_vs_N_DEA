e1670fc1c20404bf26c03af9d2512f17dcb7e782
nonname001 <austin2004@gmail.com>
2021-07-27 19:18:13 -0400

    Added GSEA and added extra variables to DE

---
+++ GSEAenricher.R
@@ -0,0 +1,105 @@
+# Credits: Tai Wang
+
+do_GSEA <- function (top_input, pvcutoff, gmt, group_name, Protein.ID.Column=NULL) {
+  
+  library (clusterProfiler)
+  library (org.Hs.eg.db)
+  library (dplyr)
+  library (data.table)
+  
+  prot.id.col <- eval(parse(text=paste0("top_input$",Protein.ID.Column)))
+  genelist <- top_input [!prot.id.col=="" | is.na(prot.id.col),]
+  gn.total <- eval(parse(text=paste0("genelist$",Protein.ID.Column)))
+  genelist <- genelist[genelist$P.Value <= pvcutoff,]
+  
+  #DE.output.test1$top2 [DE.output.test1$top2$P.Value<0.1,] -> genelist ## for debug
+  #genelist <- genelist [!(genelist$entrezgene_id=="" | is.na(genelist$entrezgene_id)),]
+  #entrezgene.col <- "entrezgene_id" ## for debug
+  #gn.total <- eval(parse(text=paste0("DE.output.test1$top2$",entrezgene.col)))
+  
+  
+  genelist <- genelist[order (genelist$logFC, decreasing = T),]
+  gl <- genelist$logFC
+  
+  #if (is.null(entrezgene.col)) {
+  #  gn <- rowid2name (row.names(genelist), targetformat = "entrezgene",useonly1st = T)
+  #  genelist <- data.frame (genelist, gn, stringsAsFactors = F)
+  #  names (gl) <- gn
+  #}
+  
+  
+  #if (!is.null(entrezgene.col)) {
+  gn <- eval(parse(text=paste0("genelist$",Protein.ID.Column)))
+  #gn.total <- eval(parse(text=paste0("top_input$",entrezgene.col)))
+  names (gl) <- gn
+  #}
+  
+  ## Treat proteinGroups that has more than 1 Protein ID:
+  ## This will add entry with multiple Protein IDs into GMT databases, the ont terms is based on pooled ont terms of the multiple EntrezGenes
+  
+  gene_split <- strsplit (names (gl), split = ";|\\|")
+  pos_split <- sapply (gene_split, length) > 1
+  p <- names (gl) [pos_split] # Identify proteingroups with multiple Protein IDs
+  
+  isinGMT <- any (gmt$gene %in% unlist (strsplit (p, split = ";|\\|")))
+  
+  if (any(pos_split) & isinGMT) {
+    ont.sel <- sapply (p, function (x) { #For each PG, find the ont terms and pool terms, adding the multiple EntrezGenes IDs (e.g 654483;107282092;552900) as Gene name in GMT
+      y <- unlist (strsplit(x, split=";|\\|"))
+      ont.sel <- unique (as.character (gmt [gmt$gene %in% y, "ont"]))
+      return (ont.sel)
+    }) 
+    
+    library (qdapTools)
+    df <- list2df(ont.sel, col1="ont",col2="gene")
+    #gmt <- gmt[!gmt$gene %in% unique (unlist (strsplit(df$gene, split=";"))),]
+    gmt.2 <- rbind (gmt,df)
+    gmt.2 <- gmt.2[order(gmt.2$ont),]
+    #  gmt.3 <- gmt.2[!gmt.2$gene %in% unlist (strsplit(p, split=";")), ]
+  } else {
+    gmt.2 <- gmt
+  }
+  
+  
+  ## If there are duplicated gene names in the geneset, keep the one with the highest abs(t)
+  order.abs.gl <- order (abs (gl), decreasing = T)
+  pos_dup <-  gl[order.abs.gl] %>% names %>% duplicated
+  val.gl.dup <- gl[order.abs.gl] [pos_dup]
+  gl <- gl [!gl %in% val.gl.dup] ## This assumes the values are unique to each gene
+  name.gl.dup <- names (gl[order.abs.gl] [pos_dup])
+  
+  #gmt.3 <- gmt.2 [!grepl (";",gmt.2$gene),]
+  # droplevels(gmt.3) -> gmt.3
+  
+  gmt.2 <- gmt.2 [gmt.2$gene %in% gn.total,] ###!!!!!!!!!!!
+  
+  #print (genelist)
+  print (gl)
+  print (length (gl))
+  
+  
+  em2 <- GSEA(gl, TERM2GENE = gmt.2, pAdjustMethod = "BH", nPerm=10000, minGSSize = 2, maxGSSize = 999999, seed = 123, pvalueCutoff = 1)
+  #em2 <- enricher(names (gl), TERM2GENE = gmt.3, universe = gn.total, pAdjustMethod = "BH",minGSSize = 1, maxGSSize = 9999,  pvalueCutoff = 1)
+  #em2.readable <- setReadable(em2, OrgDb = org.Hs.eg.db, keyType = "ENTREZID")
+  
+  #y <- as.data.frame (em2.readable)
+  y <- as.data.frame (em2)
+  y$Group <- NA
+  y$Group <- group_name
+  
+  ce <- strsplit(y$core_enrichment, split ="\\/")
+  
+  # Convert UniProt ID from core_enrichment into Shown.ID and pass into 'core_enrichment_shown_id' column
+  prot.id.col <- eval(parse(text=paste0("top_input$",Protein.ID.Column)))
+  
+  core_enrichment_shown_id <- sapply (ce, function (x){
+    pos <- match (x, prot.id.col)
+    gs <- top_input$Shown.ID[pos]
+    if (length (gs)==0) {gs==""}
+    return (paste0(gs, collapse = "/"))
+  })
+  
+  y$core_enrichment_shown_id <- core_enrichment_shown_id
+  
+  return (list(em=em2, genelist=genelist, gl=gl, df=y))
+}
--- main.r
+++ main.r
@@ -5,24 +5,28 @@ library(tidyverse)
 #   install.packages("BiocManager")
 # BiocManager::install("limma")
 library(limma)
+source("GSEAenricher.R")
 
 matrixx <- readRDS("pgdata_matrix_2.rds") # AD vs N dataset only contains AD data??
-metadata <- readRDS("pgdata_metadata.rds")
+metadata <- readRDS("pgdata_metadata_2.rds")
 doe <- read_tsv("manifest2.tsv")
 
-Gender <- factor(make.names(doe$Gender)) # dummy variable
+Gender <- factor(make.names(doe$Gender))
+Age <- doe$AGE
+Braak <- factor(make.names(doe$Braak.stage))
+Batch <- factor(make.names(doe$Gel.Batch))
 
-design <- model.matrix(~0+Gender)
+design <- model.matrix(~0+Gender+Age+Braak+Batch)
 contrast <- makeContrasts(GenderComparison = GenderFemale - GenderMale, levels = design)
 
 fit <- lmFit(matrixx, design) # what do these do?
-fit <- eBayes(fit)
+# fit <- eBayes(fit)
 fit2 <- contrasts.fit(fit, contrast) # what is the difference between fit and fit2?
 fit2 <- eBayes (fit2)
 
-top <- topTable(fit, coef = 1, adjust.method = "BH",n=Inf, sort.by = "P")
-top2 <- topTable(fit2, coef = 1, adjust.method = "BH",n=Inf, sort.by = "P")
-top <- merge(top, metadata, by=0, all=TRUE)
+# top <- topTable(fit, coef = "GenderComparison", adjust.method = "BH",n=Inf, sort.by = "P")
+top2 <- topTable(fit2, coef = "GenderComparison", adjust.method = "BH",n=Inf, sort.by = "P")
+# top <- merge(top, metadata, by=0, all=TRUE)
 top2 <- merge(top2, metadata, by=0, all=TRUE)
 
 top2_sig <- top2 %>%
@@ -32,3 +36,9 @@ print (summary(decideTests(fit,adjust.method = "BH", p.value = 0.05)))
 print (summary(decideTests(fit,adjust.method = "none", p.value = 0.05)))
 print (summary(decideTests(fit2,adjust.method = "BH", p.value = 0.05)))
 print (summary(decideTests(fit2,adjust.method = "none", p.value = 0.05)))
+
+write.table(top2_sig, file='sig.tsv', quote=FALSE, sep='\t', col.names = NA)
+
+load("gmt-go.Rdata")
+
+g1 <- do_GSEA(top2, 0.05, gmt.go, group_name = "F_vs_M", Protein.ID.Column ="Protein.IDs")
