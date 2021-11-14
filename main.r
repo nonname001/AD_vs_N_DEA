fd741dde8518d84391c595921a84a15629a1d661
nonname001 <austin2004@gmail.com>
2021-11-13 20:07:04 -0500

    Added comparison plot between different settings

--- main.r
+++ main.r
@@ -37,6 +37,7 @@ run_dea_pea <- function(matrix_file, metadata_file, doe_file, index, save_path_d
   top2 <- merge(top2, metadata, by=0, all=TRUE)
   top2$Shown.ID <- top2$hgnc_symbol
   top2$Shown.ID[top2$Shown.ID==""] <- top2$Protein.IDs[top2$Shown.ID==""]
+  top2$setting <- index
   
   top_tables[[index]] <<- top2
   
@@ -53,6 +54,8 @@ run_dea_pea <- function(matrix_file, metadata_file, doe_file, index, save_path_d
   load("gmt-go.Rdata")
   
   g1 <- do_GSEA(top2, 1, gmt.go, group_name = "F_vs_M", Protein.ID.Column ="Protein.IDs")
+  
+  g1$df$setting <- index
   
   g1_filtered <- g1$df %>%
     filter(pvalue < 0.05)
@@ -118,3 +121,66 @@ run_dea_pea("pgdata_matrix_1.rds", "pgdata_metadata_1.rds", "manifest2.tsv", "fi
 run_dea_pea("pgdata_matrix_2.rds", "pgdata_metadata_2.rds", "manifest2.tsv", "filter_bpca", "dea_2.html", "pea_2.html")
 run_dea_pea("pgdata_matrix_3.rds", "pgdata_metadata_3.rds", "manifest2.tsv", "nofilter_mindet", "dea_3.html", "pea_3.html")
 run_dea_pea("pgdata_matrix_4.rds", "pgdata_metadata_4.rds", "manifest2.tsv", "nofilter_bpca", "dea_4.html", "pea_4.html")
+
+# -------------------------
+
+# process top tables
+
+top2_all <- rbind(top_tables[["filter_mindet"]], top_tables[["filter_bpca"]], top_tables[["nofilter_mindet"]], top_tables[["nofilter_bpca"]])
+
+# process ggplot
+
+gp_all <- rbind(pea_tables[["filter_mindet"]]$df, pea_tables[["filter_bpca"]]$df, pea_tables[["nofilter_mindet"]]$df, pea_tables[["nofilter_bpca"]]$df)
+
+hcal_dea <- max ((length (unique (top2_all$Protein.IDs)) * 16.5 + 25 + 10 + 100), 500)
+hcal_pea <- max ((length (unique (gp_all$ID)) * 16.5 + 25 + 10 + 100), 500)
+#gp.pt <- ggplotly(p, tooltip = "text", height = hcal, source="DEAPlotSource") %>% 
+#  layout (yaxis=(list(automargin = F)), margin=list (l=200))
+
+# ggplot(top2_sig, aes(y=Protein.IDs, x=logFC)) + 
+#   geom_point(aes(size=P.Value, color=AveExpr)) + 
+#   labs(y="Protein IDs", x="logFC", color="Average expression", size="P-value") +
+#   theme(axis.text.x=element_text(angle=45, hjust=1), axis.text.y=element_text(size = 8, angle = 0, hjust = 1, face = "plain"))
+
+p1 <- ggplot (data=top2_all, aes (x=setting, y=Protein.IDs)) +
+  geom_point(aes (size=AveExpr, fill=logFC, color=-log10(P.Value), stroke=0.5, text=paste('<b>Log fold change:</b>', logFC, '<br>',
+                                                                                          '<b>Average expression:</b>', AveExpr,'<br>',
+                                                                                          '<b>t-statistic:</b>', t,'<br>',
+                                                                                          '<b>P-value:</b>',P.Value,'<br>',
+                                                                                          '<b>Negative log10-P-value:</b>',-log10(P.Value),'<br>',
+                                                                                          '<b>B-statistic:</b>',B,'<br>'))) +
+  scale_fill_gradient2(limits=c(-max(abs(top2_sig$logFC)), max(abs(top2_sig$logFC))), low = "#0199CC", mid = "white", high = "#FF3705", midpoint = 0) +
+  scale_color_gradient2(limits=c(0, max(abs(log10(top2_sig$P.Value)))), low = "white", mid = "white", high = "black", midpoint = 1) +
+  theme_minimal () + xlab("Comparison") + ylab("") + 
+  theme(axis.text.x=element_text(angle=45, hjust=1), axis.text.y=element_text(size = 8, angle = 0, hjust = 1, face = "plain"))
+
+gp.pt.1 <- ggplotly(p1, tooltip = "text", height = hcal_dea) %>% 
+  layout (yaxis=(list(automargin = F)), margin=list (l=200))
+
+saveWidget(gp.pt.1, "dea_all.html")
+
+# ggsave(save_path_dea)
+
+# ggplot(g1_filtered, aes(y=ID, x=rank)) + 
+#   geom_point(aes(size=pvalue, color=enrichmentScore)) + 
+#   labs(y="Protein IDs", x="logFC", color="Enrichment score", size="P-value") +
+#   theme(axis.text.x=element_text(angle=45, hjust=1), axis.text.y=element_text(size = 8, angle = 0, hjust = 1, face = "plain"))
+
+p2 <- ggplot (data=gp_all, aes (x=setting, y=ID, customdata=paste0 (gp_all$Pathway,"%sep%", gp_all$core_enrichment))) +
+  geom_point(aes (size=setSize, fill=NES, color=-log10(pvalue), stroke=0.5, text=paste('<b>Core_enrichment:</b>', core_enrichment, '<br>',
+                                                                                       '<b>Comparison:</b>', Group,'<br>',
+                                                                                       '<b>Setsize:</b>', setSize,'<br>',
+                                                                                       '<b>NES:</b>',NES,'<br>',
+                                                                                       '<b>pvalue:</b>',pvalue,'<br>',
+                                                                                       '<b>rank:</b>',rank,'<br>',
+                                                                                       '<b>leading_edge:</b>',leading_edge,'<br>'))) +
+  scale_fill_gradient2(limits=c(-max(abs(gp_all$NES)), max(abs(gp_all$NES))), low = "#0199CC", mid = "white", high = "#FF3705", midpoint = 0) +
+  scale_color_gradient2(limits=c(0, max(abs(log10(gp_all$pvalue)))), low = "white", mid = "white", high = "black", midpoint = 1) +
+  theme_minimal () + xlab("Comparison") + ylab("") + 
+  theme(axis.text.x=element_text(angle=45, hjust=1), axis.text.y=element_text(size = 8, angle = 0, hjust = 1, face = "plain"))
+
+gp.pt.2 <- ggplotly(p2, tooltip = "text", height = hcal_pea) %>% 
+  layout (yaxis=(list(automargin = F)), margin=list (l=200))
+
+saveWidget(gp.pt.2, "pea_all.html")
+
